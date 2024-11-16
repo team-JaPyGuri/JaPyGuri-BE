3aa670eb04706216a9b078ff8ac151d3481b649c
hykk-git <gratia1206@naver.com>
2024-11-16 22:43:15 +0900

    fix: get_user_id 함수 반환값 수정

--- nailo_BE/utils.py
+++ nailo_BE/utils.py
@@ -2,12 +2,14 @@ from .models import Customers, Shops
 
 def get_user_id(user_type, user_id):
     """
-    user_type과 user_id를 기반으로 Customer 또는 Shop 객체를 반환
+    user_type과 user_id를 기반으로 (user객체, user_type) 튜플을 반환
     """
     try:
         if user_type == 'customer':
-            return Customers.objects.get(customer_id=user_id)
+            user = Customers.objects.get(customer_id=user_id)
+            return (user, 'customer') 
         elif user_type == 'shop':
-            return Shops.objects.get(shop_id=user_id)
+            user = Shops.objects.get(shop_id=user_id)
+            return (user, 'shop')  
     except (Customers.DoesNotExist, Shops.DoesNotExist):
-        return None
+        return None, None 
